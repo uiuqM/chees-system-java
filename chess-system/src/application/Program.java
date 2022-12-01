1402ddd194b3fba327a7c73aee03e218848390a5
uiuqM <wilque.coelho@mail.uft.edu.br>
2022-12-01 17:36:08 -0300

    movendo pecas

--- chess-system/src/application/Program.java
+++ chess-system/src/application/Program.java
@@ -1,12 +1,27 @@
 package application;
 
+import java.util.Scanner;
+
+import chess.ChessPiece;
+import chess.ChessPosition;
 import chess.chessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		chessMatch chessmatch = new chessMatch();
-		UI.printBoard(chessmatch.getPieces());
+		while(true) {
+			UI.printBoard(chessmatch.getPieces());
+			System.out.println();
+			System.out.println("Source: ");
+			ChessPosition source = UI.readChessPosition(sc);
+			System.out.println();
+			System.out.println("Target: ");
+			ChessPosition target = UI.readChessPosition(sc);
+			
+			ChessPiece capturedPiece = chessmatch.performChessMove(source, target);
+		}
 	}
 
 }
