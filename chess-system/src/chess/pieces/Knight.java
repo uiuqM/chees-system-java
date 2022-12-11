5bd0f8302b49033c4fcb61dfc44d46f40bdd19bb
uiuqM <wilque.coelho@mail.uft.edu.br>
2022-12-11 16:11:05 -0300

    Adding other pieces

---
+++ chess-system/src/chess/pieces/Knight.java
@@ -0,0 +1,21 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Knight extends ChessPiece{
+	public Knight(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "K";
+	}
+	@Override
+	public boolean[][] possibleMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ chess-system/src/chess/pieces/Queen.java
@@ -0,0 +1,22 @@
+package chess.pieces;
+
+import boardgame.Board;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Queen extends ChessPiece {
+	public Queen(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "Q";
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
